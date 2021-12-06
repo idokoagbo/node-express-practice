7cbdf4bf62dbc16daef5ddc7763b9d3e791b96ee
Idoko Agbo <idokojudge@gmail.com>
2021-12-05 19:52:24 -0500

    removed DB hardcoded credentials, changed to env variables

--- db_config.js
+++ db_config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    user: 'me',
-    host: 'localhost',
-    database: 'api',
-    password: 'password',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 };
